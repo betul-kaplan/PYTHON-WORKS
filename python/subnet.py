06a05f87cbde8489ac5aad2c33244e81f1f7cc7f
BETUL KAPLAN <100989180+betul-kaplan@users.noreply.github.com>
2022-04-22 23:18:38 +0300

    Add files via upload

---
+++ python/subnet.py
@@ -0,0 +1,19 @@
+# What is the subnetwork address for a host with IP address 200.10.5.68/28?
+
+
+def network_id(ipaddress) :
+  ex = ipaddress.split("/")
+  ip = ex[0]
+  cidr = int(ex[1])
+  list_ip = list(map(int,ip.split(".")))
+  octet = cidr // 8
+  kalan = cidr % 8
+  list_ip[octet] = 2**(8-kalan)  * (list_ip[octet] // 2**(8-kalan))
+  for i in range(octet+1,4) :
+    list_ip[i] = 0
+  
+  return ".".join(list(map(str,list_ip)))
+
+
+ip = "200.10.5.68/12"
+print(network_id(ip))
